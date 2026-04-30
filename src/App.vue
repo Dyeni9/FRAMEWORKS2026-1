c8d1da5e8f57ca02d16789fd647753117c8e79f1
Dyenifer <166853545+Dyeni9@users.noreply.github.com>
2026-04-29 21:02:27 -0400

    Inicio onde insere dados

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
-Teste....
+<script setup>
+import { ref } from 'vue'
+
+const novoItem = ref('')
+const cesta = ref([])
+</script>
+
+<template>
+  <div class="container">
+    <h1>Gerenciador de Compras</h1>
+
+    <input 
+      v-model="novoItem" 
+      placeholder="Digite o produto..."
+    >
+    <button>Adicionar</button>
+  </div>
+</template>
+
+<style scoped>
+.container { font-family: sans-serif; max-width: 400px; margin: 20px auto; }
+</style>
